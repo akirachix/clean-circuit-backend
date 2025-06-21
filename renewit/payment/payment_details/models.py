f247073db74ee4453397b3f4673085081ee53c36
sharonmuriithi <sharonnyaguthiimuriithi@gmail.com>
2025-06-21 15:39:24 +0300

    Added changes

--- renewit/payment/payment_details/models.py
+++ renewit/payment/payment_details/models.py
@@ -7,8 +7,8 @@ class PaymentDetails(models.Model):
     type = models.CharField(max_length=50)
     quantity = models.IntegerField()
     condition = models.CharField(max_length=50)
-    # listed_at = models.DateTimeField(auto_now_add=True)
+    listed_at = models.DateTimeField(auto_now_add=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
-    # def __str__(self):
-    #     return f"PaymentDetails(payment_id={self.payment_id}, trader_id={self.trader_id}, upcycler_id={self.upcycler_id}, type={self.type}, quantity={self.quantity}, condition={self.condition}, listed_at={self.listed_at}, price={self.price})"
+    def __str__(self):
+        return f"PaymentDetails(payment_id={self.payment_id}, trader_id={self.trader_id}, upcycler_id={self.upcycler_id}, type={self.type}, quantity={self.quantity}, condition={self.condition}, listed_at={self.listed_at}, price={self.price})"
