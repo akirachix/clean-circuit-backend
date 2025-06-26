1935ae265a6c0859747ef14c670313c28e865f54
esther <esthernnyamburaa@gmail.com>
2025-06-26 09:41:09 +0300

    deleted apps

--- renewit/payment/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import PaymentDetails
-admin.site.register(PaymentDetails)
--- renewit/trader/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Trader, Material
-
-# Register your models here.
-admin.site.register(Trader)
-admin.site.register(Material)
