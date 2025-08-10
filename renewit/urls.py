b55653cda9adc5cfb1efa1c716088bde8a7cb832
esther <esthernnyamburaa@gmail.com>
2025-08-10 19:50:44 +0300

    added image placeholder to enable the frontend to add and hold images

--- renewit/urls.py
+++ renewit/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+
 from django.urls import path ,include
+
 from django.urls import path,include
 from django.http import HttpResponse
-from django.conf import settings
 from django.conf.urls.static import static
-
+from django.conf import settings
 def home(request):
     return HttpResponse("Welcome to RenewIt!")
 
@@ -28,6 +29,7 @@ def home(request):
 urlpatterns = [
     path('', home, name='home'),  
     path('admin/', admin.site.urls),
+
     path('api/', include('api.urls')),
 ]
 if settings.DEBUG:
