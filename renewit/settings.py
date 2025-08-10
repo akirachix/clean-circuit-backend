0404c1bba678f0c8d8f29ce718c1936e92ab9e8d
esther <esthernnyamburaa@gmail.com>
2025-08-10 19:44:21 +0300

    added image placeholder to enable the frontend to add and hold images

--- renewit/settings.py
+++ renewit/settings.py
@@ -60,6 +60,9 @@ INSTALLED_APPS = [
     'rest_framework.authtoken'
 ]
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
--- renewit/urls.py
+++ renewit/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-
 from django.urls import path ,include
-
 from django.urls import path,include
 from django.http import HttpResponse
+from django.conf import settings
+from django.conf.urls.static import static
 
 def home(request):
     return HttpResponse("Welcome to RenewIt!")
@@ -28,8 +28,9 @@ def home(request):
 urlpatterns = [
     path('', home, name='home'),  
     path('admin/', admin.site.urls),
-
     path('api/', include('api.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
     
